e6e3308b0f1280ea98a0e210252ce8d13cd4c2c8
Marcos Silva <89165879+MarcosSilva2021@users.noreply.github.com>
2024-11-29 21:21:46 -0300

    exibir apenas 1 gráfico

---
+++ dc_crossfilter/js/script0-apenas_1_grafico.js
@@ -0,0 +1,37 @@
+d3.csv("https://gist.githubusercontent.com/emanueles/d8df8d875edda71aa2e2365fae2ce225/raw/1e949d3da02ed6caa21fe3a7a12a4e5a611a4bab/stocks.csv").then(function (data) {
+    // formatando os dados
+    let parseDate = d3.timeParse("%Y/%m/%d");
+    data.forEach(function (d)  {
+        d.date = parseDate(d.date); // formatando a base de dados - convertendo
+        d.google = +d.google;
+        d.facebook = +d.facebook;
+    });
+
+    // Criando uma instancia de crossfilter
+    let ndx = crossfilter(data);
+
+    // criando as dimensões e grupos
+    let dateDimension = ndx.dimension((d) => d.date);
+
+    let googleByDayGroup = dateDimension.group().reduceSum((d) => d.google);
+
+    let lineChart = dc.lineChart(document.querySelector("#chart"));
+    let xScale = d3
+        .scaleTime()
+        .domain([dateDimension.bottom(1)[0].date, dateDimension.top(1)[0].date]);
+
+    lineChart
+        .width(800)
+        .height(400)
+        .dimension(dateDimension)
+        .margins({top: 30, right: 50, bottom: 25, left: 40})
+        .renderArea(false)
+        .x(xScale)
+        .xUnits(d3.timeDays)
+        .renderHorizontalGridLines(true)
+        .legend(dc.legend().x(680).y(10).itemHeight(13).gap(5))
+        .brushOn(false)
+        .group(googleByDayGroup, "Google");
+
+    dc.renderAll();
+});
